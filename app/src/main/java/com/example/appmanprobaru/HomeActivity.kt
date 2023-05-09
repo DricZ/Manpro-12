0e2d7c83553a56361e980a8b3e7f0f7602e25b82
Joy <joykaihatu13@gmail.com>
2023-05-09 11:55:03 +0700

    Fragment swap di HomeActivity
    -Bottom Navbar sudah bisa dipakai untuk live dan home button

--- app/src/main/java/com/example/appmanprobaru/HomeActivity.kt
+++ app/src/main/java/com/example/appmanprobaru/HomeActivity.kt
@@ -3,14 +3,45 @@ package com.example.appmanprobaru
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.example.appmnprobaru.btn_home_active
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-@Suppress("DEPRECATION")
+
 class HomeActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_screen_with_navbar)
+        loadFragment(btn_home_active())
+        bottomNav = findViewById<BottomNavigationView>(R.id.bot_nav_static) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.bottom_navbar_home->{
+                    loadFragment(btn_home_active())
+                    true
+                }
+                R.id.bottom_navbar_live->{
+                    loadFragment(btn_live_active())
+                    true
+                }
+                R.id.bottom_navbar_events ->{
+                    true
+                }
+                R.id.bottom_navbar_profile->{
+                    true
+                }
+                else ->{
+                    true
+                }
+            }
+        }
 
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.Main_fragment,fragment)
+        transaction.commit()
     }
 
 }
