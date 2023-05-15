8045a4cad11d834e635da6deaf6e2c38593e651c
MichaelChristianChandra <c14200013@john.petra.ac.id>
2023-05-15 23:31:09 +0700

    App backend temp

--- app/src/main/java/com/example/appmanprobaru/LoginActivity.kt
+++ app/src/main/java/com/example/appmanprobaru/LoginActivity.kt
@@ -1,18 +1,25 @@
 package com.example.appmanprobaru
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import android.text.SpannableString
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 @Suppress("DEPRECATION")
 class LoginActivity : AppCompatActivity() {
+    lateinit var db: FirebaseFirestore
+    private var _userN : MutableList<String> = emptyList<String>().toMutableList()
+    private var _pass : MutableList<String> = emptyList<String>().toMutableList()
+    private var _id : MutableList<String> = emptyList<String>().toMutableList()
     private lateinit var et_email: EditText
     private lateinit var et_password: EditText
     private lateinit var button_login: Button
@@ -41,6 +48,22 @@ class LoginActivity : AppCompatActivity() {
         }
 
 
+        db = FirebaseFirestore.getInstance()
+        var dbAccount = db.collection("account")
+
+        dbAccount.get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    _userN.add(document.data["username"].toString())
+                    _pass.add(document.data["password"].toString())
+                    _id.add(document.id.toString())
+                    Log.d("CEK DATA", "CEK ${_userN}")
+                    Log.d("GET DATA", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("GET DATA", "Error getting documents: ", exception)
+            }
 
         button_login.setOnClickListener {
             login()
@@ -50,22 +73,39 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun login(){
-
+        var cek = false
         val email = et_email.text.toString()
         val pass = et_password.text.toString()
+        val sharedPreferences = getSharedPreferences("SessionUser", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
         if (email.isBlank() || pass.isBlank()){
             Toast.makeText(this, "Username and Password cant be blank", Toast.LENGTH_SHORT).show()
         }
-        else if (email == "tes123" && pass == "123"){
-            Toast.makeText(this, "Welcome!", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-        else if (email != "tes123" && pass != "123"){
-            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+        else{
+            for (x in 0.._userN.size-1){
+                if (email.equals(_userN[x].toString()) and pass.equals(_pass[x].toString())){
+                    cek = true
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Login Success!",
+                        Toast.LENGTH_SHORT
+                    ).show()
 
+                    editor.putString("id_user", _id[x].toString())
+                    editor.apply()
+                    val eIntent = Intent(this@LoginActivity, HomeActivity::class.java)
+                    startActivity(eIntent)
+              }
+            }
+            if (!cek){
+                Toast.makeText(
+                    this@LoginActivity,
+                    "Email atau Password Salah!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+
     }
 
 
--- app/src/main/java/com/example/appmanprobaru/home_page_recyclerView_Data.kt
+++ app/src/main/java/com/example/appmanprobaru/home_page_recyclerView_Data.kt
@@ -2,6 +2,12 @@ package com.example.appmanprobaru
 
 import android.widget.Button
 
-data class home_page_recyclerView_Data(val titleImage: Int, val titleText:String){
-
-}
+data class home_page_recyclerView_Data(
+    val titleImage: Int,
+    val Name:String,
+    val desc:String,
+    val category:String,
+    val date:String,
+    val location:String,
+    val maxPeserta:String
+    )
