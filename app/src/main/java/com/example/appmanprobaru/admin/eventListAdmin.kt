130d51f958a56daad02634ade10c55beeb6cd423
BudimanCandra <104149766+BudimanCandra@users.noreply.github.com>
2023-05-23 01:49:39 +0700

    Front end admin

---
+++ app/src/main/java/com/example/appmanprobaru/admin/eventListAdmin.kt
@@ -0,0 +1,12 @@
+package com.example.appmanprobaru.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.appmanprobaru.R
+
+class eventListAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_list_admin)
+    }
+}
