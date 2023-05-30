81d5b14d8a919dc3fb9591f56dd46968b0c5326d
Audrico <67388541+DricZ@users.noreply.github.com>
2023-05-30 08:41:39 +0700

    fix navbar

--- app/src/main/java/com/example/appmanprobaru/admin/adminListAdmin.kt
+++ app/src/main/java/com/example/appmanprobaru/admin/adminListAdmin.kt
@@ -3,44 +3,62 @@ package com.example.appmanprobaru.admin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.appmanprobaru.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-class adminListAdmin : AppCompatActivity() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [adminListAdmin.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class adminListAdmin : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_list_admin)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-        var _navbarAdmin = findViewById<BottomNavigationView>(R.id.navbarAdmin) as BottomNavigationView
-        _navbarAdmin.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.bottom_navbar_home -> {
-                    val intent = Intent(this@adminListAdmin, HomeAdmin::class.java)
-                    startActivity(intent)
-                    true
-                }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.activity_admin_list_admin, container, false)
 
-                R.id.bottom_navbar_people -> {
-                    val intent = Intent(this@adminListAdmin, PeopleAdmin::class.java)
-                    startActivity(intent)
-                    true
-                }
+        return view
+    }
 
-                R.id.bottom_navbar_events -> {
-                    val intent = Intent(this@adminListAdmin, eventListAdmin::class.java)
-                    startActivity(intent)
-                    true
-                }
-
-                R.id.bottom_navbar_profileadm -> {
-
-                    true
-                }
-
-                else -> {
-                    true
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment btn_home_active.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            adminListAdmin().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
             }
-        }
     }
 }
--- app/src/main/java/com/example/appmanprobaru/admin/eventListAdmin.kt
+++ app/src/main/java/com/example/appmanprobaru/admin/eventListAdmin.kt
@@ -1,76 +1,64 @@
-package com.example.appmanprobaru.admin
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appmanprobaru.R
+import com.example.appmanprobaru.admin.HomeAdmin
+import com.example.appmanprobaru.admin.HomeEvent
+import com.example.appmanprobaru.admin.adapterEventAdmin
+import com.example.appmanprobaru.admin.addEvent
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-
-class eventListAdmin : AppCompatActivity() {
+class eventListAdmin : Fragment() {
     private lateinit var _rvHomeEventAdmin: RecyclerView
 
     private lateinit var datalist: ArrayList<HomeEvent>
 
-    private var _id : MutableList<String> = emptyList<String>().toMutableList()
+    private var _id: MutableList<String> = emptyList<String>().toMutableList()
 
-    private var _name : MutableList<String> = emptyList<String>().toMutableList()
-    private var _img : MutableList<String> = emptyList<String>().toMutableList()
-    private var _desc : MutableList<String> = emptyList<String>().toMutableList()
-    private var _location : MutableList<String> = emptyList<String>().toMutableList()
-    private var _date : MutableList<String> = emptyList<String>().toMutableList()
-    private var _category : MutableList<String> = emptyList<String>().toMutableList()
-    private var _maxPeserta : MutableList<String> = emptyList<String>().toMutableList()
-    private var _kategoriPeserta : MutableList<String> = emptyList<String>().toMutableList()
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_list_admin)
-        datalist =  arrayListOf<HomeEvent>()
+    private var _name: MutableList<String> = emptyList<String>().toMutableList()
+    private var _img: MutableList<String> = emptyList<String>().toMutableList()
+    private var _desc: MutableList<String> = emptyList<String>().toMutableList()
+    private var _location: MutableList<String> = emptyList<String>().toMutableList()
+    private var _date: MutableList<String> = emptyList<String>().toMutableList()
+    private var _category: MutableList<String> = emptyList<String>().toMutableList()
+    private var _maxPeserta: MutableList<String> = emptyList<String>().toMutableList()
+    private var _kategoriPeserta: MutableList<String> = emptyList<String>().toMutableList()
 
-        _rvHomeEventAdmin = findViewById<RecyclerView>(R.id.rvEvent)
-        val _btnAddAdmin = findViewById<Button>(R.id.addAdmin)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.activity_event_list_admin, container, false)
+        datalist = arrayListOf<HomeEvent>()
+
+        _rvHomeEventAdmin = view.findViewById<RecyclerView>(R.id.rvEvent)
+        val _btnAddAdmin = view.findViewById<Button>(R.id.addAdmin)
 
         _btnAddAdmin.setOnClickListener {
-            val intent = Intent(this@eventListAdmin, addEvent::class.java)
-            startActivity(intent)
+            val newFragment = addEvent()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.Main_fragment_admin, newFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
         }
 
-        var _navbarAdmin = findViewById<BottomNavigationView>(R.id.navbarAdmin) as BottomNavigationView
-        _navbarAdmin.setOnItemSelectedListener {
-            when (it.itemId){
-                R.id.bottom_navbar_home->{
-                    val intent = Intent(this@eventListAdmin, HomeAdmin::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.bottom_navbar_people->{
-                    val intent = Intent(this@eventListAdmin, PeopleAdmin::class.java)
-                    startActivity(intent)
-                    true
-                }
-                R.id.bottom_navbar_events ->{
 
-                    true
-                }
-                R.id.bottom_navbar_profileadm->{
-
-                    true
-                }
-                else ->{
-                    true
-                }
-            }
-        }
         datainit()
+        return view
     }
-    private fun datainit(){
-//        val layoutManager = GridLayoutManager(2)
+
+    private fun datainit() {
+// val layoutManager = GridLayoutManager(2)
 
         val db = Firebase.firestore
         var dbevent = db.collection("event")
@@ -80,15 +68,23 @@ class eventListAdmin : AppCompatActivity() {
                     _id.add(document.id.toString())
                     _name.add(document.data["name"].toString())
                     _img.add(document.data["imgloc"].toString())
-                    _date.add(((document.data["date"] as com.google.firebase.Timestamp).toDate().toString()))
+                    _date.add(
+                        ((document.data["date"] as com.google.firebase.Timestamp).toDate().toString())
+                    )
                 }
-                for (x in 0.._id.size-1){
-                    val arrayDate: List<String> =_date[x].split(" ")
-                    val eventdata =HomeEvent(_id[x], _img[x],_name[x],arrayDate[1] + " "+arrayDate[2] + " " + arrayDate[5],arrayDate[3] + " "+arrayDate[4])
+                for (x in 0.._id.size - 1) {
+                    val arrayDate: List<String> = _date[x].split(" ")
+                    val eventdata = HomeEvent(
+                        _id[x],
+                        _img[x],
+                        _name[x],
+                        arrayDate[1] + " " + arrayDate[2] + " " + arrayDate[5],
+                        arrayDate[3] + " " + arrayDate[4]
+                    )
                     datalist.add(eventdata)
 
                 }
-                _rvHomeEventAdmin.layoutManager = LinearLayoutManager(this)
+                _rvHomeEventAdmin.layoutManager = LinearLayoutManager(context)
                 _rvHomeEventAdmin.adapter = adapterEventAdmin(datalist)
 
 
